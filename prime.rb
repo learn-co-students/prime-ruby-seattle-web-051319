e4283c468f7759657a259f3120e249d222d6f69f
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-29 22:47:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(i)
+  if i <= 0 || i == 1
+    return false
+  else
+  (2..(i -1)).to_a.all? {|number| i % number != 0}
+  end
+  # below is another way(the first way I solved this lab. above is what I was shown after asking a question about the lab.)
+  # (2..(i -1)).each do |number|
+  #   return false if i % number == 0
+  # end
+  # true
+end
