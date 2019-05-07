a26517313297ea91f3d18fa39844b2d6009c7a5a
Danielle Martinoli <daniechris5@gmail.com>
2019-05-07 01:19:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,7 +1,8 @@
-def prime?(int)
-  if int == prime?
-    return "true"
-  else
-    return "false"
+def prime?(n)
+  if n / 1 || n == integer
+    return true
+  end
+  if n / 2 == integer
+    return false
   end
 end
